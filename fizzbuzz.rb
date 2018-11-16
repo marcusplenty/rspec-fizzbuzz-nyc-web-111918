e1a885787e06b7476b1a3d42d8253187569dea14
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-16 22:31:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0 
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "buzz"
+  else
+    return nil 
+  end
+end
